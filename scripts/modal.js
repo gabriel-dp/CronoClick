6c4a6c9e87cfbca4efcb7318f42adb02886b74e2
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-04-06 09:29:14 -0300

    :recycle: Refactor styles and modal script

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,4 +1,11 @@
-export function openModal() {
+function openModal() {
 	const modalBackground = document.getElementsByClassName("modal")[0].parentElement;
-	console.log(modalBackground);
+	modalBackground.style.visibility = "visible";
+	modalBackground.style.opacity = 1;
+}
+
+function closeModal() {
+	const modalBackground = document.getElementsByClassName("modal")[0].parentElement;
+	modalBackground.style.visibility = "hidden";
+	modalBackground.style.opacity = 0;
 }
