2e118b69318c9bcf53b569bc8542356777e05bf7
Bernardo Maia Detomi <bernardomd01@aluno.ufsj.edu.br>
2025-04-29 23:44:36 -0300

    ✨ added error messages

--- src/components/forms/SignUpForm/types.ts
+++ src/components/forms/SignUpForm/types.ts
@@ -2,13 +2,15 @@ import z from "zod";
 
 export const signUpSchema = z
 	.object({
-		username: z.string().trim().min(1),
-		password: z.string().regex(/^[\S]{8,32}$/),
-		passwordConfirm: z.string(),
-		email: z.string().email()
+		username: z.string().trim().min(1, "O usuário é obrigatório"),
+		password: z
+			.string()
+			.regex(/^[\S]{8,32}$/, "A senha deve ter entre 8 e 32 caracteres"),
+		passwordConfirm: z.string().min(1, "A confirmação de senha é obrigatória"),
+		email: z.string().email("Email inválido").min(1, "O email é obrigatório")
 	})
 	.refine((data) => data.password === data.passwordConfirm, {
-		message: "Passwords don't match",
+		message: "As senhas não coincidem",
 		path: ["passwordConfirm"]
 	});
 
