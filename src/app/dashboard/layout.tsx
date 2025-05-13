ea9a61ebada376b8165ddf4c352c9fd09c17ccb5
Bernardo Maia Detomi <bernardomd01@aluno.ufsj.edu.br>
2025-05-13 09:02:13 -0300

    :sparkles: Add dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DashboardLayout({
+	children
+}: {
+	children: React.ReactNode;
+}) {
+	return <>{children}</>;
+}
--- src/components/forms/SignUpForm/types.ts
+++ src/components/forms/SignUpForm/types.ts
@@ -6,8 +6,13 @@ export const signUpSchema = z
 		password: z
 			.string()
 			.regex(/^[\S]{8,32}$/, "A senha deve ter entre 8 e 32 caracteres"),
-		passwordConfirm: z.string().min(1, "A confirmação de senha é obrigatória"),
-		email: z.string().email("Email inválido").min(1, "O email é obrigatório")
+		passwordConfirm: z
+			.string()
+			.min(1, "A confirmação de senha é obrigatória"),
+		email: z
+			.string()
+			.email("Email inválido")
+			.min(1, "O email é obrigatório")
 	})
 	.refine((data) => data.password === data.passwordConfirm, {
 		message: "As senhas não coincidem",
