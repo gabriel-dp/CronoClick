76d7ebb882812bb59ed0d009814302e51ce10ab6
BernardoMD <bernardomd01@gmail.com>
2025-05-22 10:40:40 -0300

    :sparkles: Add redirect to schedule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,47 @@
 import { useSession } from "next-auth/react";
 //import { useRouter } from "next/navigation";
 
-//import Button from "@/components/ui/Button";
+import Button from "@/components/ui/Button";
 
 import { HomeMainContainer } from "./styles";
-import DashboardPage from "./dashboard/page";
+//import DashboardPage from "./dashboard/page";
+import { DashboardContainer } from "./dashboard/styles";
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
+
 
 export default function Home() {
-	const session = useSession();
-	//const { push } = useRouter();
-	return (
-		<HomeMainContainer>
-			{session.status == "authenticated" && (
-				// <Button onClick={() => push("/schedule")}>
-				// 	Ver Cronogramas
-				// </Button>
-				<DashboardPage></DashboardPage>
-			)}
-		</HomeMainContainer>
+	const { data: session, status} = useSession();
+	const { push } = useRouter();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (status != "authenticated"){
+			<DashboardContainer>
+			<h1>Bem-vindo ao CronoClick!</h1>
+			<p>Cadastre sua conta para acessar todos os conteúdos disponiveis no CronoClick!</p>
+				<Button onClick={() => push("/sign-up")}>
+				 	Crie sua conta
+				</Button>
+			</DashboardContainer>
+		}
+	}, [status, router]);
+
+	if (status === "loading") {
+		return <DashboardContainer>Carregando...</DashboardContainer>
+	}
+
+	if (!session) {
+		return null;
+	}
+	
+	return(
+		<DashboardContainer>
+			<h1>Bem-vindo ao CronoClick!</h1>
+			<p>Se login foi realizado com sucesso.</p>
+			<Button onClick={() => push("/schedule")}>
+				Ver Cronogramas
+			</Button>
+		</DashboardContainer>
 	);
-}
+}	
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -7,4 +7,13 @@ export const HomeMainContainer = styled.main`
 		margin: auto;
 		margin-top: 2rem;
 	}
+		h1 {
+		color: ${(props) => props.theme.primary};
+		font-size: 2.5rem;
+	}
+
+	p {
+		font-size: 1.25rem;
+		color: ${(props) => props.theme.text};
+	}
 `;
