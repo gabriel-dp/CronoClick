c04ff3f3a270bdd07821fe55a140eae3f23534da
BernardoMD <bernardomd01@gmail.com>
2025-06-12 09:04:32 -0300

    :sparkles: Add logo to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import Logo from "@/assets/logo.svg";
+
+import Image from "next/image";
+
 import Button from "@/components/ui/Button";
 
 import { HomeMainContainer } from "./styles";
@@ -18,6 +22,7 @@ export default function Home() {
 	if (status !== "authenticated") {
 		return (
 			<HomeMainContainer>
+				<Image src={Logo} alt="Logo" />
 				<h1>Bem-vindo ao CronoClick!</h1>
 				<p>
 					Cadastre sua conta para acessar todos os conteúdos
@@ -30,6 +35,7 @@ export default function Home() {
 
 	return (
 		<HomeMainContainer>
+			<Image src={Logo} alt="Logo" />
 			<h1>Bem-vindo ao CronoClick!</h1>
 			<p>Seu login foi realizado com sucesso.</p>
 			<Button onClick={() => push("/schedule")}>Ver Cronogramas</Button>
--- src/app/styles.ts
+++ src/app/styles.ts
@@ -2,6 +2,14 @@
 
 import styled from "styled-components";
 
+import Logo from "@/assets/logo.svg";
+
+export const StyledLogo = styled(Logo)`
+	width: 100px;
+	height: auto;
+	margin-bottom: 1rem;
+`;
+
 export const HomeMainContainer = styled.main`
 	min-height: calc(100dvh - 9rem);
 	padding: 2rem;
